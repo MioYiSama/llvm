706c77d5b389f7c4050be2f4aebb995536505485
MioYiSama <mioyisama@gmail.com>
2025-08-10 09:40:35 +0800

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 VERSION=$1
 
-echo "Building version $VERSION"
+gh release download "llvmorg-$VERSION" -R "llvm/llvm-project" -p "llvm-project-$VERSION.src.tar.xz"
+
+tar xf "llvm-project-$VERSION.src.tar.xz" --directory="llvm"
+
+ls "llvm"
